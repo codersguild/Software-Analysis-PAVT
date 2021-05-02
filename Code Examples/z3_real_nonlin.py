afc5ad1e2213568049a1b9b23733f862521296eb
Sumit Lahiri <lahiri.devs@gmail.com>
2021-05-02 09:24:28 +0530

    Create z3_real_nonlin.py

---
+++ Code Examples/z3_real_nonlin.py
@@ -0,0 +1,23 @@
+import z3
+w s = z3.Real("w_sym s_sym")
+
+S = z3.Solver()
+S.add(w * s == 6)
+S.set("timeout", 5000)
+
+if S.check() == z3.sat 
+  m = S.model()
+  
+  w_frac = m[w].as_fraction()
+  value = float((w_frac.numerator)) / float(
+      (w_frac.denominator)
+  )
+  
+  print("\t%s = %.10f\n" % (w, round(value, 10)))
+  
+  s_frac = m[s].as_fraction()
+  value = float((s_frac.numerator)) / float(
+      (s_frac.denominator)
+  )
+  
+  print("\t%s = %.10f\n" % (s, round(value, 10)))
