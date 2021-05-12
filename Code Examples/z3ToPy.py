735ec03d71a9d1710a683f23b01b1356701f1755
Sumit Lahiri <lahiri.devs@gmail.com>
2021-05-13 00:31:22 +0530

    Update z3ToPy.py

--- Code Examples/z3ToPy.py
+++ Code Examples/z3ToPy.py
@@ -9,6 +9,7 @@ z3.set_option(
   max_visited=1000000,
 )
     
+# https://z3prover.github.io/api/html/classz3py_1_1_rat_num_ref.html#a1012d6314d35530c58f9c018269ec867
 def num(r):
     """
     Convert from Z3 to python float values.
@@ -17,10 +18,15 @@ def num(r):
 
 
 def get_value(r):
+    # https://stackoverflow.com/questions/12598408/z3-python-getting-python-values-from-model/12600208
     """
     Convert from Z3 to python values.
     """
-    if z3.is_int_value(r):
+    if z3.is_true(r):
+        return z3.is_true(r)
+    elif z3.is_false(r):
+        return z3.is_false(r)
+    elif z3.is_int_value(r):
         return r.as_long()
     elif z3.is_algebraic_value(r):
         return round(num(r.approx(15)), 10)
@@ -30,6 +36,13 @@ def get_value(r):
         None
     else:
         return num(r)
-      
-# # https://z3prover.github.io/api/html/classz3py_1_1_rat_num_ref.html#a1012d6314d35530c58f9c018269ec867
+ 
+"""
+Z3 ITE
+VALUE = z3.If(
+   CONDITION,
+   VALUE_IF_TRUE,
+   VALUE_IF_FALSE,
+)
+"""
 
