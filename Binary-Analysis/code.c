9278d27ae485695a0fa0a52e534f3f82cf535fdd
Sumit Lahiri <lahiri.devs@gmail.com>
2021-07-30 01:26:05 +0530

    Create code.c

---
+++ Binary-Analysis/code.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+
+// https://blog.grimm-co.com/2020/05/guided-fuzzing-with-driller.html
+int main(int argc, char *argv[]) {
+  char buffer[6] = {0};
+  int i;
+  int *null = 0;
+
+  read(0, buffer, 6);
+  if (buffer[0] == '7' && buffer[1] == '/' && buffer[2] == '4'
+      && buffer[3] == '2' && buffer[4] == 'a' && buffer[5] == '8') {
+    i = *null;
+    assert(0);
+  }
+
+  puts("No problem");
+  return 0;
+}
